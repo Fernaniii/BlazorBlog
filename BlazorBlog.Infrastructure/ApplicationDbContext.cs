7c04e491fe9d8631417063366f099d2daae39848
Aces_Fernan <fernando.magnaye@acescredit.ph>
2026-02-20 14:42:30 +0800

    Added Infrastructure Layer

---
+++ BlazorBlog.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using BlazorBlog.Domain.Articles;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBlog.Infrastructure
+{
+    public  class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options) 
+        {
+            
+        }
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
---
+++ BlazorBlog.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using BlazorBlog.Application.Articles;
+using BlazorBlog.Domain.Articles;
+using BlazorBlog.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorBlog.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            // Register the Repoisitory in Here
+            services.AddScoped<IArticleRepository, ArticleRepository>();
+
+            return services;
+        }
+    }
+}
