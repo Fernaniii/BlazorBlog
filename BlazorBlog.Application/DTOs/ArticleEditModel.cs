f3b9dec6a17b827dce26d1c1536af17d9f7143e7
Aces_Fernan <fernando.magnaye@acescredit.ph>
2026-03-12 17:29:14 +0800

    - Added Article View model for Updating Articles Data Safely

---
+++ BlazorBlog.Application/DTOs/ArticleEditModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBlog.Application.DTOs
+{
+    public class ArticleEditModel
+    {
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string? Content { get; set; }
+
+        public bool IsPublished { get; set; }
+    }
+}
---
+++ BlazorBlog/ViewModels/ArticleFormModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBlog.WebUI.Server.ViewModels
+{
+    public class ArticleFormModel
+    {
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string? Content { get; set; }
+    }
+}
