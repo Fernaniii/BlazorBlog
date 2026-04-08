a9d44072f6c97051e50d161a05cb56035299b4e6
Aces_Fernan <fernando.magnaye@acescredit.ph>
2026-04-08 11:29:36 +0800

    - Added Domain Tests unit

---
+++ BlazorBlog.Domain.Tests/ArticleTests.cs
@@ -0,0 +1,80 @@
+﻿using BlazorBlog.Domain.Articles;
+using FluentAssertions;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBlog.Domain.ArticleTests
+{
+    public class ArticleTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Create_WithValidData_ShouldInitializedCorrectly()
+        {
+            // Arrange
+            var title = "Test Article";
+            var content = "Test Content";
+
+
+            // Act
+            var article = Article.Create(title, content);
+
+
+            // Assert
+            article.Should().NotBeNull();
+            article.Title.Should().Be(title);
+            article.Content.Should().Be(content);
+            article.IsPublished.Should().BeFalse();
+
+            article.PublishedOn.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(5));
+        }
+
+        [Test]
+        public void Create_WithEmptyTitle_ShouldThrowArgumentException()
+        {
+            // Arrange
+            var emptyTitle = "";
+            var content = "Test Content";
+
+            // Act
+            var ex = Assert.Throws<ValidationException>(() => Article.Create(emptyTitle, content));
+
+            // When using Argument Exception, we can check the message directly without needing to catch the exception
+            //Action act = () => Article.Create(emptyTitle, content);
+
+            // Assert
+            Assert.That(ex.Message, Does.Contain("Title cannot be empty"));
+
+
+            //When using argument exception, we can check the message directly without needing to catch the exception
+            //act.Should().Throw<ArgumentException>()
+            //    .WithMessage("*Title*");
+        }
+
+
+        [Test]
+        public void Create_WithNullTitle_ShouldThrowArgumentException()
+        {
+            // Arrange
+            string nullTitle = null!;
+            var content = "Test Content";
+
+            // Act
+            var ex = Assert.Throws<ValidationException>(() => Article.Create(nullTitle, content));
+            //Action act = () => Article.Create(nullTitle, content);
+
+            Assert.That(ex.Message, Does.Contain("Title cannot be empty"));
+
+            // Assert
+            //act.Should().Throw<ArgumentException>()
+            //    .WithMessage("*Title*");
+        }
+
+
+
+
+        }
+}
