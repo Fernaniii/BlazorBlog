c09122076b5f61dc45585ab2c0d8383fbed1b930
Aces_Fernan <fernando.magnaye@acescredit.ph>
2026-04-10 16:34:50 +0800

    Management page(Log view page)  1. Create interface in BlazorApp.Application.Abstraction name it IAuditRepository  2. Create a Repository implementation in the BlazorApp.Infrastructure.Repositories  3. Register the Service in the Infrastructure.DependencyInjection.cs  4. Also create a service implementation in the Application layer calling the Repository to connect to database  5. Register the service in the ..Application.DependencyInjection Build

---
+++ BlazorBlog.Application/DTOs/AuditChanges.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBlog.Application.DTOs
+{
+    public class AuditChanges
+    {
+        public Dictionary<string, object?> Old { get; set; } = new();
+        public Dictionary<string, object?> New { get; set; } = new();
+    }
+}
---
+++ BlazorBlog.Infrastructure/Repositories/AuditRepository.cs
@@ -0,0 +1,27 @@
+﻿using BlazorBlog.Application.Abstractions;
+using BlazorBlog.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBlog.Infrastructure.Repositories;
+
+public class AuditRepository : IAuditRepository
+{
+
+    private readonly ApplicationDbContext _context;
+
+    public AuditRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<AuditLog>> GetLogsAsync()
+    {
+        return await _context.AuditLogs
+             .AsNoTracking()
+             .OrderByDescending(x => x.Timestamp)
+             .ToListAsync();
+    }
+}
